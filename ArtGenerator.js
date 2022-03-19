5ede5cdd7d3c4904f97e06dd460571b93bdef007
Matthew Wilber <mwilber@gmail.com>
2022-03-18 21:03:55 -0400

    cleanup

--- ArtGenerator.js
+++ ArtGenerator.js
@@ -7,17 +7,13 @@ class ArtGenerator {
 
 		// Draw a sample message to the canvas...
 		// Set the background color
-		this.ctx.fillStyle = "#eeeeff";
+		this.ctx.fillStyle = "#000000";
 		// Draw a filled rectangle the same size as the canvas
 		this.ctx.fillRect(0, 0, canvas.height, canvas.width);
-		// Set the text color
-		this.ctx.fillStyle = "#333399";
-		// Draw filled text
-		this.ctx.fillText("Time to Draw Something!", 10, 20, canvas.width-20);
 	}
 
 	DrawPixel (x, y, color) {
-		ctx.fillStyle = color;
-		ctx.fillRect(Math.round(x), Math.round(y), 1, 1);
+		this.ctx.fillStyle = color;
+		this.ctx.fillRect(Math.round(x), Math.round(y), 1, 1);
 	}
 }
